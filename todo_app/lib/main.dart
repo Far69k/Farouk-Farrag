97fd1ba55f07016d2ecd7bc276c7505fb48d8f45
Farouk Mosalam <135360048+Far69k@users.noreply.github.com>
2023-09-20 02:29:34 +0300

    Update main.dart

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'mainscreen.dart';
 import 'user_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   SharedPreferences prefs = await SharedPreferences.getInstance();
   final todoList = TodoList(prefs);
   runApp(
@@ -14,7 +16,7 @@ void main() async {
       create: (context) => todoList,
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: mainScreen(),
+        home: auth(),
       ),
     ),
   );
